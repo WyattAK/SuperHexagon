a2c43eaf56dcba5c500752f1e7b6cf5488e3a795
Wyatt <82850335+WyattAK@users.noreply.github.com>
2021-04-19 20:33:52 -0400

    Super Hexagon C# Scripts
    All C# scripts used in the making of the game Super Hexagon.

---
+++ StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+	public void StartAbout()
+	{
+		SceneManager.LoadScene("AboutMenu");
+	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("SuperHexagon");
+	}
+		
+}
